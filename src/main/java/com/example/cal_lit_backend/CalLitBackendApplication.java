a8846bcb3e40f63bc133292f47243d44fae0277e
officialsachin <sachin@litschool.in>
2026-07-14 15:15:27 +0530

    app fully configured

---
+++ src/main/java/com/example/cal_lit_backend/CalLitBackendApplication.java
@@ -0,0 +1,15 @@
+package com.example.cal_lit_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalLitBackendApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CalLitBackendApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/example/cal_lit_backend/CalLitBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cal_lit_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalLitBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
